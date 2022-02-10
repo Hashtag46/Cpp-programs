e1f9accf817ae5e9f861c795fe0bd02b0f8dd561
Hashtag46 <89920792+Hashtag46@users.noreply.github.com>
2022-02-10 21:18:32 +0530

    Update factorise.cpp

--- factorise.cpp
+++ factorise.cpp
@@ -6,6 +6,7 @@ int main()
     int num,i;
     cout<<"Enter the number: ";
     cin>>num;
+    cout<<"The factors are: ";
     for(i=1;i<=num;i++)
     {
         if (num%i==0) ///to isolate only the even factors, just give && i%2==0
